4da644671d1d797557a3999c85d672b260d6145f
Vlad Oksentiuk <vlad.oksentuk@keenethics.com>
2018-11-29 18:25:42 +0200

    Refactored Textarea. Component -> Function. Experimented with props

--- client/components/form/Textarea.jsx
+++ client/components/form/Textarea.jsx
@@ -1,49 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import nanoid from 'nanoid';
 
-export default class Textarea extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
-  render() {
-    const {
+const Textarea = ({
+  id,
+  className,
+  name,
+  value,
+  placeholder,
+  rows,
+  onChange,
+  isDisabled,
+  isInvalid,
+  isValid,
+}) => (
+  <textarea
+    {...{
       id,
+      rows,
       name,
-      value,
       placeholder,
       onChange,
-    } = this.props;
-
-    return (
-      <textarea
-        id={id}
-        className="textarea"
-        rows={2}
-        name={name || ''}
-        placeholder={placeholder || ''}
-        onChange={onChange}
-      >
-        {value}
-      </textarea>
-    );
-  }
-}
+      ...(onChange ? { value } : { defaultValue: value }),
+      className: classNames({
+        [className]: true,
+        disabled: isDisabled,
+        invalid: isInvalid,
+        valid: isValid,
+      }),
+    }}
+  />
+);
 
 Textarea.propTypes = {
   id: PropTypes.string,
+  className: PropTypes.string,
   placeholder: PropTypes.string,
   name: PropTypes.string,
   value: PropTypes.string,
+  rows: PropTypes.number,
   onChange: PropTypes.func,
+  isDisabled: PropTypes.bool,
+  isInvalid: PropTypes.bool,
+  isValid: PropTypes.bool,
 };
 Textarea.defaultProps = {
-  id: `textfield-${nanoid(8)}`,
+  id: `textarea-${nanoid(8)}`,
+  className: 'textarea',
   placeholder: '',
   name: '',
   value: '',
+  rows: 2,
   onChange: null,
+  isDisabled: false,
+  isInvalid: false,
+  isValid: false,
 };
+
+export default Textarea;
