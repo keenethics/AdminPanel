23b03a8444c832a32787fbcca80b8db2f9c5c4c5
Gornostaev Sergey <svgornostaev@gmail.com>
2018-12-20 00:26:21 +0200

    Add assets folder, add node environment in script, fix filenames

--- client/routing/Routes.jsx
+++ client/routing/Routes.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Index from 'Pages/index';
-import Components from 'Pages/components';
+import Index from 'Pages/Index';
+import Components from 'Pages/Components';
 import NoMatch from 'Pages/NoMatch';
 
 const Routes = () => (
--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ const methodOverride = require('method-override');
 const routes = require('./routes/index.route');
 
 const app = express();
+const mode = app.get('env');
+const isProduction = mode && mode === 'production';
 
 app.use(cors());
 app.use(helmet());
@@ -17,10 +19,10 @@ app.use(methodOverride());
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(express.static('dist'));
-
+app.use('/assets', express.static('assets'));
 app.use('/api', routes);
 
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '../client/index.html')));
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, `../${isProduction ? 'dist' : 'client'}/index.html`)));
 
 const port = process.env.APP_PORT || 3001;
 const host = process.env.APP_HOST || 'localhost';
