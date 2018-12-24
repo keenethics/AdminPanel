c359f5b7ca8671d80e091f1badafc4b4e06d4666
Vlad Oksentiuk <vlad.oksentuk@keenethics.com>
2018-12-24 16:54:46 +0200

    Delete app.jsx

--- client/app.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-import Routes from './routing/Routes';
-
-const App = () => <Routes />;
-
-render(<App />, document.getElementById('root'));
