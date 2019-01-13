f79d3341f76f42521e4cb313038862e600851ddd
Vlad Oksentiuk <vlad.oksentuk@keenethics.com>
2019-01-13 23:42:19 +0200

    Add password field, creation and comparison to User model

--- server/migrations/20190108115854-create-user.js
+++ server/migrations/20190108115854-create-user.js
@@ -12,6 +12,10 @@ module.exports = {
     email: {
       type: Sequelize.STRING,
     },
+    password: {
+      allowNull: false,
+      type: Sequelize.STRING,
+    },
     createdAt: {
       allowNull: false,
       type: Sequelize.DATE,
--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,24 @@
+const bcrypt = require('bcryptjs');
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
+    password: DataTypes.STRING,
   }, {});
   User.associate = (models) => { // eslint-disable-line
     // associations can be defined here
   };
+
+  User.beforeCreate(async (user) => {
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(user.password, salt); // eslint-disable-line
+  });
+
+  User.prototype.comparePassword = function comparePassword(password) {
+    return bcrypt.compare(password, this.password);
+  };
+
   return User;
 };
