48cfc89d3f443a6708a33d6a28dac182f32cef36
Vlad Oksentiuk <vlad.oksentuk@keenethics.com>
2019-03-04 21:15:42 +0200

    Use before/after hooks to clean up db

--- server/tests/auth.test.js
+++ server/tests/auth.test.js
@@ -9,35 +9,30 @@ describe('POST /user', () => {
     password: 'test',
   };
 
-  it('should create new user and return right data without sensitive info', async () => {
-    await cleanUsersExceptDefaultOne();
+  beforeEach(() => cleanUsersExceptDefaultOne());
+  afterEach(() => cleanUsersExceptDefaultOne());
 
-    return request
-      .post(`${apiBase}/user`)
-      .send(newUser)
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(201)
-      .expect((res) => {
-        if (res.body.email !== newUser.email) {
-          throw new Error('Emails don\'t match!');
-        }
+  it('should create new user and return right data without sensitive info', () => request
+    .post(`${apiBase}/user`)
+    .send(newUser)
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .expect(201)
+    .expect((res) => {
+      if (res.body.email !== newUser.email) {
+        throw new Error('Emails don\'t match!');
+      }
 
-        // sensitive fields: [password, refresh_token]
-        if (res.body.password || res.body.refresh_token) {
-          throw new Error('Sensitive user info is sent to client!');
-        }
-      });
-  });
+      // sensitive fields: [password, refresh_token]
+      if (res.body.password || res.body.refresh_token) {
+        throw new Error('Sensitive user info is sent to client!');
+      }
+    }));
 
-  it('should not create user without password', async () => {
-    await cleanUsersExceptDefaultOne();
-
-    return request
-      .post(`${apiBase}/user`)
-      .send({ email: newUser.email })
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(500);
-  });
+  it('should not create user without password', () => request
+    .post(`${apiBase}/user`)
+    .send({ email: newUser.email })
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .expect(500));
 });
