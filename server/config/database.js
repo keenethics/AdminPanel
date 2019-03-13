20198757badb2bf0eb8a39be82c2ee7cac5d537a
Gornostaev Sergey <svgornostaev@gmail.com>
2019-03-13 13:42:45 +0200

    Update database config
    Sequelize - String based operators are now deprecated. Please use Symbol based operators for better security.

--- server/config/database.js
+++ server/config/database.js
@@ -3,11 +3,14 @@ require('dotenv-safe').config(); // Required for sequelize-cli to load .env
 module.exports = {
   development: {
     use_env_variable: 'DATABASE_URL',
+    operatorsAliases: false,
   },
   test: {
     use_env_variable: 'TEST_DATABASE_URL',
+    operatorsAliases: false,
   },
   production: {
     use_env_variable: 'DATABASE_URL',
+    operatorsAliases: false,
   },
 };
