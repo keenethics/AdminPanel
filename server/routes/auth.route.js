48e0808c027a8c445149f6200b8e75f6e23e53ad
Dmytro Panontko <dmytro.panontko@keenethics.com>
2019-04-26 16:18:46 +0300

    Marging 'swagger' and 'jsdoc' #40

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -3,12 +3,77 @@ const authCtrl = require('../controllers/auth.controller');
 
 const router = express.Router();
 
+/**
+* @swagger
+* /api/auth/login:
+*   post:
+*     tags:
+*       - Auth
+*     name: Login
+*     consumes:
+*       - application/json
+*     parameters:
+*       - name: body
+*         in: body
+*         schema:
+*           type: object
+*           properties:
+*             email:
+*               type: string
+*             password:
+*               type: string
+*               format: password
+*         required:
+*           - email
+*           - password
+*     responses:
+*       201:
+*         description: Without message
+*       401:
+*         description: Wrong email or password | Unauthorized
+*       500:
+*         schema:
+*           type: object
+*           properties:
+*             error:
+*               type: string
+*/
+
 router.route('/login')
   .post(
     authCtrl.authenticate,
     authCtrl.generateAccessToken,
     authCtrl.returnAccessToken,
   );
+
+/**
+* @swagger
+* /api/auth/refresh:
+*   post:
+*     tags:
+*       - Auth
+*     name: Refresh
+*     consumes:
+*       - application/json
+*     parameters:
+*       - name: body
+*         in: body
+*         schema:
+*           type: object
+*           properties:
+*             email:
+*               type: string
+*             refresh_token:
+*               type: string
+*         required:
+*           - email
+*           - refresh_token
+*     responses:
+*       201:
+*         description: User found and logged in successfully
+*       401:
+*         description: Invalid email or refresh_token | Unauthorized
+*/
 
 router.route('/refresh')
   .post(
