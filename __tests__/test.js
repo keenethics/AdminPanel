574e85969ae0f5994e28ed548e0d7e18cf0c8062
Gornostaev Sergey <svgornostaev@gmail.com>
2019-05-28 10:32:22 +0300

    Minor fix

--- __tests__/test.js
+++ __tests__/test.js
@@ -2,15 +2,15 @@
 
 describe('Sample tests', () => {
   it('should display title on components page', async () => {
-    await page.goto('http://localhost:3005/form-components');
+    await page.goto('http://localhost:3006/form-components');
     await expect(page).toMatch('Form components');
   });
   it('should display title on signin page', async () => {
-    await page.goto('http://localhost:3005/signin');
+    await page.goto('http://localhost:3006/signin');
     await expect(page).toMatch('Sign in');
   });
   it('should display title on signup page', async () => {
-    await page.goto('http://localhost:3005/signup');
+    await page.goto('http://localhost:3006/signup');
     await expect(page).toMatch('Sign up');
   });
 });
